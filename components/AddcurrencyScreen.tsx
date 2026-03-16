8212f0b50a2cd40908f15bb7f44cfbcaca027e4f
DGgemini <dariusgreeff@gmail.com>
2026-03-16 22:58:40 +0200

    added dropdown

--- components/AddcurrencyScreen.tsx
+++ components/AddcurrencyScreen.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet,View, Text, Modal, Pressable } from 'react-native'
 import React from 'react'
+import { Picker } from '@react-native-picker/picker';
 
 type addCurrencyScreenProps = {
     visible: boolean;
     onClose: () => void;
 }
+const currencies = ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "SEK", "NZD"];
 
 const AddcurrencyScreen = ({ visible, onClose }: addCurrencyScreenProps) => {
   return (
@@ -13,12 +15,16 @@ const AddcurrencyScreen = ({ visible, onClose }: addCurrencyScreenProps) => {
         <Pressable onPress={onClose} style={{width:"100%", height:"100%", display:"flex", justifyContent:"center", alignItems:"center"}}>
             <Pressable onPress={() => {}}  style={styles.Content}> 
                 <View>
-                    <Text>add currency screen</Text>
+                    <Text style={{fontSize:20, fontWeight:"bold", margin:10, textDecorationLine:"underline"}}>Add Currency</Text>
+                    <Text>From</Text>
+                    <Picker>
+                        {currencies.map((currency) => (
+                            <Picker.Item label={currency} value={currency} key={currency} />
+                        ))}
+                    </Picker>
+                    <Text>To</Text>
                 </View>
             </Pressable>
-           
-     
-
         </Pressable>
         </View>
     </Modal>
@@ -42,7 +48,7 @@ const styles = StyleSheet.create({
         backgroundColor:"white",
         borderRadius:10,
         display:"flex",
-        justifyContent:"center",
+        justifyContent:"space-between",
         alignItems:"center"
     }
 
